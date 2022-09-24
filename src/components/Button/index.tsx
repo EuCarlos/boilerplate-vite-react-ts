00535cb6dffa42edab91fb0e8713790d9e4d365a
Carlos Alves <carlosalves369m@hotmail.com>
2022-09-24 15:48:41 +0000

    :memo: create documentation with `@storybook/react`
    add storybook dependencies and fix Button component path

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import ButtonS from './button';
+
+interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <ButtonS
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </ButtonS>
+  );
+};
--- src/components/SayHello/Button/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ButtonStyle } from './styles'
-
-export function Button() {
-	return <ButtonStyle color='#fff'>Read me</ButtonStyle>
-}
